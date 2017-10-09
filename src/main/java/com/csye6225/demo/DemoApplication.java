aadf720bb3131f14dab8a38d4bf14d7870bd1e51
Riddhichabhadia <chabhadia.r@husky.neu.edu>
2017-10-09 14:25:04 -0400

    corrected the merge

---
+++ src/main/java/com/csye6225/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.csye6225.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
